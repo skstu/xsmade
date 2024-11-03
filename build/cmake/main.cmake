57c264fa6554149d2192a118aa90226aeea071d2
memade <k34ubll5@outlook.com>
2024-11-03 23:00:29 +0800

    init

---
+++ build/cmake/main.cmake
@@ -0,0 +1,8 @@
+#############################################################################
+# Name:        build/cmake/main.cmake
+# Purpose:     Main CMake file
+# Author:      Martell
+# Created:     2024-11-03
+# Copyright:   (c) 2024 skstu development team
+# Licence:     skstu licence
+#############################################################################
---
+++ include/xs/version.h
@@ -0,0 +1,12 @@
+#if !defined(__FB50D306_393A_45BE_8303_C3336CF0F3E0__)
+#define __FB50D306_393A_45BE_8303_C3336CF0F3E0__
+
+#define xsMAJOR_VERSION      1
+#define xsMINOR_VERSION      0
+#define xsRELEASE_NUMBER     0
+#define xsSUBRELEASE_NUMBER  0
+
+/// /*_ Memade®（新生™） _**/
+/// /*_ Sun, 03 Nov 2024 14:48:12 GMT _**/
+/// /*_____ https://www.skstu.com/ _____ **/
+#endif///__FB50D306_393A_45BE_8303_C3336CF0F3E0__
