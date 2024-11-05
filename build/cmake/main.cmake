73601dfd7280a43d04d8c6788863eb4172634697
memade <k34ubll5@outlook.com>
2024-11-05 11:47:06 +0800

    updated

--- build/cmake/main.cmake
+++ build/cmake/main.cmake
@@ -7,18 +7,10 @@
 # Licence:     skstu licence
 #############################################################################
 
-list(APPEND CMAKE_MODULE_PATH "${xsSOURCE_DIR}/build/cmake/modules")
+list(APPEND CMAKE_MODULE_PATH "build/cmake/modules")
 
 include(global)
 include(build/cmake/functions.cmake)        # skstu functions
-#[[
-include(build/cmake/files.cmake)            # Files list
-include(build/cmake/source_groups.cmake)    # Source group definitions
-include(build/cmake/functions.cmake)        # wxWidgets functions
-include(build/cmake/toolkit.cmake)          # Platform/toolkit settings
-include(build/cmake/options.cmake)          # User options
-include(build/cmake/init.cmake)             # Init various global build vars
-include(build/cmake/pch.cmake)              # Precompiled header support
-]]
 
-add_subdirectory(projects projects)
+add_subdirectory(projects)
+add_subdirectory(tests)
