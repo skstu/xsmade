3ba0ab0d08114652d6cf7cf7a1789b0f8e8adcae
memade <k34ubll5@outlook.com>
2024-11-06 11:32:49 +0800

    init

---
+++ include/utfcpp.hpp
@@ -0,0 +1,8 @@
+#if !defined(__ABEE6917_B5AB_4A83_A7E7_06B9082219F6__)
+#define __ABEE6917_B5AB_4A83_A7E7_06B9082219F6__
+
+
+/// /*_ Memade®（新生™） _**/
+/// /*_ Wed, 06 Nov 2024 03:12:44 GMT _**/
+/// /*_____ https://www.skstu.com/ _____ **/
+#endif///__ABEE6917_B5AB_4A83_A7E7_06B9082219F6__
--- tests/test_main/main.cc
+++ tests/test_main/main.cc
@@ -1,6 +1,27 @@
 #include <iostream>
-#include <icu.hpp>
+// #include <icu.hpp>
+#include <stl.hpp>
+#include <utf8.h>
 int main(int argc, char **argv) {
+  do { //!@ test utfcpp
+    std::string u8text = u8R"("asglkasdgh你好啊。@#￥……@……！";)";
+    std::string::iterator end_it =
+        utf8::find_invalid(u8text.begin(), u8text.end());
+    std::vector<unsigned short> utf16line;
+    utf8::utf8to16(u8text.begin(), end_it, std::back_inserter(utf16line));
+    std::u16string u16text;
+    u16text.append((char16_t *)&utf16line[0], utf16line.size());
+
+    std::string utf8line;
+    utf8::utf16to8(u16text.begin(), u16text.end(),
+                   std::back_inserter(utf8line));
+    auto kk = u16text.c_str();
+    std::cout << "u8:" << utf8line << std::endl;
+    // std::cout << "u16:" << u16text << std::endl;
+    auto __pause__ = 1;
+  } while (0);
+
+#if 0
   std::string name;
   const std::string src = "asglkasdgh你好啊。@#￥……@……！";
   Icu::Detect(src, name);
@@ -12,5 +33,6 @@ int main(int argc, char **argv) {
   memcpy((char *)&u16tmp[0], dst.data(), dst.size());
   std::cout << name << std::endl;
   std::cout << dst << std::endl;
+#endif
   return 0;
 }
