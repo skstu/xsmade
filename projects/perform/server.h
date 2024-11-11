ee9644e06d150911fff51e60428b6fa89896f0d7
memade <k34ubll5@outlook.com>
2024-11-11 20:16:09 +0800

    updated

---
+++ projects/perform/server.h
@@ -0,0 +1,13 @@
+#if !defined(__66D36CE1_C3F0_4601_8CB0_3DCE31E2D698__)
+#define __66D36CE1_C3F0_4601_8CB0_3DCE31E2D698__
+
+class Server {
+public:
+Server();
+~Server();
+
+};
+/// /*_ Memade®（新生™） _**/
+/// /*_ Mon, 11 Nov 2024 09:56:22 GMT _**/
+/// /*_____ https://www.skstu.com/ _____ **/
+#endif///__66D36CE1_C3F0_4601_8CB0_3DCE31E2D698__
