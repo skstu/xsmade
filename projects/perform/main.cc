379352fdd99f24762c788937cba568e91aca6aaf
火星熊猫 <huoxingxiongmao@huoxingxiongmaodeMac-mini.local>
2024-11-12 18:58:23 +0800

    updated

--- projects/perform/main.cc
+++ projects/perform/main.cc
@@ -2,9 +2,6 @@
 
 // launchctl submit -l com.example.hidden_app -- /path/to/hidden_app
 
-static bool open_browser();
-static bool close_browser();
-
 int main(int argc, char **argv) {
   std::string cmdline = Stl::PackageCommandLine(argc, argv);
   tfCommandLines cmdlines = Stl::ParserCommandLines(cmdline);
@@ -24,69 +21,3 @@ int main(int argc, char **argv) {
   xs_sys_process_kill(pid);
   return 0;
 }
-//  /Users/huoxingxiongmao/Desktop/skstu/xsmade/out/perform
-bool open_browser() {
-  char **environ;
-  bool result = false;
-  do {
-#if defined(__APPLE__)
-    const char *chromium_path =
-        "/Users/huoxingxiongmao/Desktop/241107.app/Contents/MacOS/chromium";
-    const char *args[] = {chromium_path, /*"--proxy-server=127.0.0.1",*/
-                          "pixelscan.net", nullptr};
-
-    pid_t pid;
-    int status;
-    if (posix_spawn(&pid, chromium_path, nullptr, nullptr,
-                    const_cast<char *const *>(args), environ) != 0)
-      break;
-#elif _WIN32
-
-#endif
-    result = true;
-  } while (0);
-  return result;
-}
-bool close_browser() {
-  bool result = false;
-  do {
-#if defined(__APPLE__)
-#elif _WIN32
-#endif
-    result = true;
-  } while (0);
-  return result;
-}
-
-#if 0 //!@ Create browser
-
-
-  // 浏览器路径和参数
-  const char *chromium_path =
-      "/Users/huoxingxiongmao/Desktop/241107.app/Contents/MacOS/chromium";
-  const char *args[] = {chromium_path, /*"--proxy-server=127.0.0.1",*/
-                      "pixelscan.net", nullptr};
-
-  pid_t pid;
-  int status;
-
-  // 使用 posix_spawn 打开浏览器
-  if (posix_spawn(&pid, chromium_path, nullptr, nullptr,
-                  const_cast<char *const *>(args), environ) == 0) {
-    // std::cout << "Browser launched successfully with PID: " << pid <<
-    // std::endl;
-
-#if 0
-    // 等待浏览器进程结束（可选）
-    if (waitpid(pid, &status, 0) != -1) {
-      // std::cout << "Browser process finished with status: " << status
-      //<< std::endl;
-    } else {
-      // std::cerr << "Failed to wait for browser process." << std::endl;
-    }
-#endif
-  } else {
-    // std::cerr << "Failed to launch browser." << std::endl;
-  }
-
-#endif
