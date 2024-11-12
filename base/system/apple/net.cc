aab8c3dfa2170153209f4a0b85dfda09879a8974
火星熊猫 <huoxingxiongmao@huoxingxiongmaodeMac-mini.local>
2024-11-12 14:08:05 +0800

    updated

--- base/system/apple/net.cc
+++ base/system/apple/net.cc
@@ -1,37 +1,27 @@
 #include "sys.h"
 
 XS_EXTERN unsigned short xs_sys_get_free_port(void) {
-#if 0
-
-
-bool Win::Tcpip::getFreePort(unsigned short& outPort) {
-    bool result = false;
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1) {
-        return false;
-    }
-
+  unsigned short result = 0;
+  int sockfd = -1;
+  do {
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1)
+      break;
     sockaddr_in addr;
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = INADDR_ANY;
-    addr.sin_port = 0;  // 让系统选择随机端口
+    addr.sin_port = 0; // 让系统选择随机端口
 
-    if (bind(sockfd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
-        close(sockfd);
-        return false;
-    }
+    if (bind(sockfd, (struct sockaddr *)&addr, sizeof(addr)) == -1)
+      break;
 
     socklen_t addrlen = sizeof(addr);
-    if (getsockname(sockfd, (struct sockaddr*)&addr, &addrlen) == -1) {
-        close(sockfd);
-        return false;
-    }
+    if (getsockname(sockfd, (struct sockaddr *)&addr, &addrlen) == -1)
+      break;
 
-    outPort = ntohs(addr.sin_port);
+    result = ntohs(addr.sin_port);
+  } while (0);
+  if (sockfd != -1)
     close(sockfd);
-    return true;
-}
-
-#endif
-  return 0;
+  return result;
 }
