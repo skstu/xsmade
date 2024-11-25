06e6cf2cf7cb750844e640ef3de506525447b3b6
memade <k34ubll5@outlook.com>
2024-11-25 19:39:02 +0800

    updated

--- build/scripts/ffmpeg-projects/win/msys2-clang64-ffmpeg.sh
+++ build/scripts/ffmpeg-projects/win/msys2-clang64-ffmpeg.sh
@@ -12,7 +12,6 @@ deps_lib_dir=${INSTALL_DIR}/lib
 deps_include_dir=${INSTALL_DIR}/include
 
 #make distclean
-# ./configure --enable-gpl --enable-version3 --enable-nonfree --enable-shared --enable-pic --enable-static --enable-small --enable-runtime-cpudetect --enable-zlib --enable-libx264 --enable-libx265 --enable-libvpx --enable-libfdk-aac --enable-libopus --enable-libvorbis --enable-libfreetype --enable-libass --enable-libmp3lame --enable-libtheora --enable-libxvid --enable-libdav1d --enable-libsnappy --enable-libwebp --enable-libshine --enable-libvidstab --enable-libsoxr --enable-libopenh264 --enable-librtmp --enable-libgsm --enable-libmysofa --enable-libspeex --enable-libbluray --enable-libmfx --enable-libzvbi --enable-libfontconfig --enable-libfribidi --enable-libcelt --enable-libmodplug --enable-libcaca --enable-libcdio --enable-libaom --enable-libsvtav1 --enable-librav1e --enable-libdavs2
 
 PKG_CONFIG_PATH=${deps_pkg_config_path}
 
@@ -23,6 +22,7 @@ bash ./configure \
 --arch=x86_64 \
 --disable-debug \
 --disable-doc \
+--enable-sdl \
 --enable-ffplay \
 --enable-decoder='mjpeg,png,aac,libx264,libx265,libmp3lame,libfaac' \
 --enable-encoder='aac,png,mjpeg,libx264,libx265,libmp3lame,libfaac' \
